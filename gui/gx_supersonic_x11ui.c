037c2def0eef0dd82dcadf74d1a8b0ecdd066e0d
Thorsten Maerz <info@netztorte.de>
2023-01-12 19:29:18 +0100

    remove old gtk workaround (reset_request())

--- gui/gx_supersonic_x11ui.c
+++ gui/gx_supersonic_x11ui.c
@@ -652,12 +652,6 @@ static void controller_expose(const gx_supersonicUI * const ui,  const gx_contro
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-// hack to allow resizing below the initial size on gtk based hosts
-static void reset_request(gx_supersonicUI * const ui) {
-	ui->resize->ui_resize(ui->resize->handle, ui->width/1.4, ui->height/1.4);
-	ui->first_resize = false;
-}
-
 // resize the xwindow and the cairo xlib surface
 static void resize_event(gx_supersonicUI * const ui) {
 	XWindowAttributes attrs;
@@ -979,7 +973,6 @@ static void event_handler(gx_supersonicUI * const ui) {
 					break;
 					case Button2:
 						// click on the mouse wheel
-						if (ui->first_resize) reset_request(ui);
 						//debug_print("Button2 \n");
 					break;
 					case Button3:
